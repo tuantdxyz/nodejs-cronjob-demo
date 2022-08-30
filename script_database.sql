bf853d576910786785adfd403aa5b7f77d5247bd
tuantd16 <tuantd16@fpt.com.vn>
2022-08-30 17:27:32 +0700

    edit script db

--- script_database.sql
+++ script_database.sql
@@ -2,9 +2,8 @@
 CREATE DATABASE `proxy_lab` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 
 -- proxy_lab.Persons definition
-
 CREATE TABLE `Persons` (
-   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `lastName` varchar(255) NOT NULL,
   `firstName` varchar(255) DEFAULT NULL,
   `address` varchar(255) DEFAULT NULL,
@@ -12,5 +11,7 @@ CREATE TABLE `Persons` (
   `status` int(11) DEFAULT NULL,
   `age` int(11) DEFAULT NULL,
   `actived` tinyint(1) DEFAULT NULL,
+  `created_date` datetime DEFAULT current_timestamp(),
+  `updated_date` datetime DEFAULT NULL ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
